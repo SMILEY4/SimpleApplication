fe74bf63698d6500387fadc25e456341457c004b
Lukas Ruegner <ruegnerlukas@gmail.com>
2019-12-19 18:59:36 +0100

    Initial Commit

---
+++ src/main/java/de/ruegnerlukas/simpleapplication/test/TestApplication.java
@@ -0,0 +1,10 @@
+package de.ruegnerlukas.simpleapplication.test;
+
+public class TestApplication {
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module de.ruegnerlukas.simpleapplication {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+//    opens de.ruegnerlukas.simpleapplication to javafx.fxml;
+//    exports de.ruegnerlukas.simpleapplication;
+}
