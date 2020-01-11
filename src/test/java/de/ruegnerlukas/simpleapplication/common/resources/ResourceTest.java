85abfd04890f3a256fdc6b97def6f7cc23317354
Lukas Ruegner <ruegnerlukas@gmail.com>
2020-01-11 10:28:48 +0100

    Remove testcase

--- src/test/java/de/ruegnerlukas/simpleapplication/common/resources/ResourceTest.java
+++ src/test/java/de/ruegnerlukas/simpleapplication/common/resources/ResourceTest.java
@@ -10,15 +10,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ResourceTest {
 
 
-	@Test
-	public void testExternal() {
-		final String rootDir = new File("").getAbsolutePath();
-		final Resource resource = Resource.external(rootDir + "\\src\\test\\resources\\testResources\\testFile.txt");
-		assertThat(resource).isNotNull();
-		assertThat(resource.asURL()).isNotNull();
-		assertThat(resource.asFile()).isNotNull();
-		assertThat(resource.asFile()).exists();
-	}
+//	@Test
+//	public void testExternal() {
+//		final String rootDir = new File("").getAbsolutePath();
+//		final Resource resource = Resource.external(rootDir + "\\src\\test\\resources\\testResources\\testFile.txt");
+//		assertThat(resource).isNotNull();
+//		assertThat(resource.asURL()).isNotNull();
+//		assertThat(resource.asFile()).isNotNull();
+//		assertThat(resource.asFile()).exists();
+//	}
 
 
 
