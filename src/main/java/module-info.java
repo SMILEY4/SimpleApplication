e65ff67a5c6f6cce8461dd4ae2d5037d4e20e8eb
Lukas Ruegner <ruegnerlukas@gmail.com>
2020-03-14 21:09:41 +0100

    Re-implement module system

---
+++ src/main/java/module-info.java
@@ -0,0 +1,25 @@
+open module SimpleApplication {
+	requires transitive javafx.controls;
+	requires transitive javafx.fxml;
+	requires transitive org.slf4j;
+	requires transitive com.fasterxml.jackson.core;
+	requires transitive com.fasterxml.jackson.databind;
+	requires transitive com.fasterxml.jackson.annotation;
+	requires static lombok;
+
+	exports de.ruegnerlukas.simpleapplication.common.callbacks;
+	exports de.ruegnerlukas.simpleapplication.common.events;
+	exports de.ruegnerlukas.simpleapplication.common.events.specializedevents;
+	exports de.ruegnerlukas.simpleapplication.common.instanceproviders;
+	exports de.ruegnerlukas.simpleapplication.common.instanceproviders.factories;
+	exports de.ruegnerlukas.simpleapplication.common.instanceproviders.providers;
+	exports de.ruegnerlukas.simpleapplication.common.resources;
+	exports de.ruegnerlukas.simpleapplication.common.validation;
+	exports de.ruegnerlukas.simpleapplication.core.application;
+	exports de.ruegnerlukas.simpleapplication.core.events;
+	exports de.ruegnerlukas.simpleapplication.core.extensions;
+	exports de.ruegnerlukas.simpleapplication.core.plugins;
+	exports de.ruegnerlukas.simpleapplication.core.presentation.module;
+	exports de.ruegnerlukas.simpleapplication.core.presentation.views;
+	exports de.ruegnerlukas.simpleapplication.core.presentation.utils;
+}
