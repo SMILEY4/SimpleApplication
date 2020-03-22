04a7d3d5c555cd1f9f824eb9c3abf6bd2a2550b7
Lukas Ruegner <ruegnerlukas@gmail.com>
2020-03-22 11:09:14 +0100

    Simplify exposed event and command

--- src/main/java/de/ruegnerlukas/simpleapplication/core/presentation/module/ExposedCommand.java
+++ src/main/java/de/ruegnerlukas/simpleapplication/core/presentation/module/ExposedCommand.java
@@ -1,25 +1,17 @@
 package de.ruegnerlukas.simpleapplication.core.presentation.module;
 
-import de.ruegnerlukas.simpleapplication.common.events.TriggerableEventSource;
 import de.ruegnerlukas.simpleapplication.common.events.Channel;
-import de.ruegnerlukas.simpleapplication.core.events.Publishable;
-import lombok.AllArgsConstructor;
+import de.ruegnerlukas.simpleapplication.core.events.PublishableEvent.PublishableEventSource;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class ExposedCommand {
 
 
 	/**
-	 * The channel of this command.
-	 */
-	private final Channel channel;
-
-	/**
 	 * The event source of this command.
 	 */
-	private final TriggerableEventSource<Publishable> eventSource;
+	private final PublishableEventSource eventSource;
 
 	/**
 	 * The scope of this command.
@@ -30,36 +22,55 @@ public class ExposedCommand {
 
 
 	/**
-	 * @param channel     the channel of this command
 	 * @param eventSource the event source of this command
 	 * @return a new {@link ExposedCommand} with the given name, source and {@link UIExtensionScope#INTERNAL} as its scope
 	 */
-	public static ExposedCommand internal(final Channel channel, final TriggerableEventSource<Publishable> eventSource) {
-		return new ExposedCommand(channel, eventSource, UIExtensionScope.INTERNAL);
+	public static ExposedCommand internal(final PublishableEventSource eventSource) {
+		return new ExposedCommand(eventSource, UIExtensionScope.INTERNAL);
 	}
 
 
 
 
 	/**
-	 * @param channel     the channel of this command
 	 * @param eventSource the event source of this command
 	 * @return a new {@link ExposedCommand} with the given name, source and {@link UIExtensionScope#LOCAL} as its scope
 	 */
-	public static ExposedCommand local(final Channel channel, final TriggerableEventSource<Publishable> eventSource) {
-		return new ExposedCommand(channel, eventSource, UIExtensionScope.LOCAL);
+	public static ExposedCommand local(final PublishableEventSource eventSource) {
+		return new ExposedCommand(eventSource, UIExtensionScope.LOCAL);
 	}
 
 
 
 
 	/**
-	 * @param channel     the channel of this command
 	 * @param eventSource the event source of this command
 	 * @return a new {@link ExposedCommand} with the given name, source and {@link UIExtensionScope#GLOBAL} as its scope
 	 */
-	public static ExposedCommand global(final Channel channel, final TriggerableEventSource<Publishable> eventSource) {
-		return new ExposedCommand(channel, eventSource, UIExtensionScope.GLOBAL);
+	public static ExposedCommand global(final PublishableEventSource eventSource) {
+		return new ExposedCommand(eventSource, UIExtensionScope.GLOBAL);
+	}
+
+
+
+
+	/**
+	 * @param eventSource the {@link PublishableEventSource} to expose
+	 * @param scope       the scope of this exposed command
+	 */
+	public ExposedCommand(final PublishableEventSource eventSource, final UIExtensionScope scope) {
+		this.eventSource = eventSource;
+		this.scope = scope;
+	}
+
+
+
+
+	/**
+	 * @return the {@link Channel} of this exposed command.
+	 */
+	public Channel getChannel() {
+		return eventSource.getChannel();
 	}
 
 
