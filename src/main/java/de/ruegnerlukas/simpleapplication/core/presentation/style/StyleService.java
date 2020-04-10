c8f9c873f2743c12393fdaefdcae74c0de283412
Lukas Ruegner <ruegnerlukas@gmail.com>
2020-04-10 17:00:16 +0200

    Implement style service

--- src/main/java/de/ruegnerlukas/simpleapplication/core/presentation/style/StyleService.java
+++ src/main/java/de/ruegnerlukas/simpleapplication/core/presentation/style/StyleService.java
@@ -3,6 +3,7 @@ package de.ruegnerlukas.simpleapplication.core.presentation.style;
 import de.ruegnerlukas.simpleapplication.common.resources.Resource;
 import javafx.scene.Node;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StyleService {
@@ -26,6 +27,7 @@ public interface StyleService {
 	 * @return the created style
 	 */
 	Style createFromString(String name, String... string);
+
 
 	/**
 	 * Registers the given style with the given name. If a style with the name already exists, it will be overwritten.
@@ -52,6 +54,16 @@ public interface StyleService {
 	 */
 	void applyStyleTo(String name, Node target);
 
+
+	/**
+	 * Applies the styles registered with the given names to the given target.
+	 *
+	 * @param names  the names of the {@link Style}s
+	 * @param target the target {@link Node}
+	 */
+	void applyStylesTo(List<String> names, Node target);
+
+
 	/**
 	 * Applies the style registered with the given name to the given target node and removes all other previously applied styles.
 	 *
@@ -71,10 +83,12 @@ public interface StyleService {
 
 
 	/**
-	 * Disconnects the node from this style service. This will remove all applied (and registered) {@link Style}s.
+	 * Disconnects the node from this style service. This will remove all applied (registered) {@link Style}s.
+	 *
 	 * @param node the {@link Node} to disconnect
 	 */
 	void disconnectNode(Node node);
+
 
 	/**
 	 * Reloads the style registered with the given name.
@@ -97,5 +111,26 @@ public interface StyleService {
 	 * @return an optional with the style (if found)
 	 */
 	Optional<Style> findStyle(String name);
+
+
+	/**
+	 * @param target the target {@link Node}
+	 * @return all styles applied to the given target node by this style service.
+	 */
+	List<Style> getAppliedStyles(Node target);
+
+
+	/**
+	 * @param target the target {@link Node}
+	 * @return all names of the styles applied to the given target node by this style service.
+	 */
+	List<String> getAppliedStyleNames(Node target);
+
+
+	/**
+	 * @param style the name of the registered {@link Style}
+	 * @return all {@link Node}s the given style was applied to.
+	 */
+	List<Node> getTargets(String style);
 
 }
--- src/main/java/de/ruegnerlukas/simpleapplication/core/presentation/style/StyleServiceImpl.java
+++ src/main/java/de/ruegnerlukas/simpleapplication/core/presentation/style/StyleServiceImpl.java
@@ -1,16 +1,41 @@
 package de.ruegnerlukas.simpleapplication.core.presentation.style;
 
 import de.ruegnerlukas.simpleapplication.common.resources.Resource;
+import de.ruegnerlukas.simpleapplication.common.validation.Validations;
 import javafx.scene.Node;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Slf4j
 public class StyleServiceImpl implements StyleService {
+
+
+	/**
+	 * All registered {@link Style}s. The name of the registered style is the key.
+	 */
+	private Map<String, Style> styles = new HashMap<>();
+
+	/**
+	 * All nodes and their applied styles.
+	 */
+	private Map<Node, List<Style>> targets = new HashMap<>();
+
+
 
 
 	@Override
 	public Style createFromResource(final String name, final Resource resource) {
-		return null;
+		Validations.INPUT.notBlank(name).exception("The name can not be null or empty.");
+		Validations.INPUT.notNull(resource).exception("The resource can not be null.");
+		final Style style = Style.fromResource(resource);
+		registerStyle(style, name);
+		return style;
 	}
 
 
@@ -18,7 +43,11 @@ public class StyleServiceImpl implements StyleService {
 
 	@Override
 	public Style createFromString(final String name, final String... string) {
-		return null;
+		Validations.INPUT.notBlank(name).exception("The name can not be null or empty.");
+		Validations.INPUT.notNull(string).exception("The style strings can not be null.");
+		final Style style = Style.fromString(string);
+		registerStyle(style, name);
+		return style;
 	}
 
 
@@ -26,7 +55,16 @@ public class StyleServiceImpl implements StyleService {
 
 	@Override
 	public void registerStyle(final Style style, final String name) {
-
+		Validations.INPUT.notNull(style).exception("The style can not be null.");
+		Validations.INPUT.notBlank(name).exception("The name can not be null or empty.");
+		if (styles.containsKey(name)) {
+			final List<Node> targets = getTargets(name);
+			deregisterStyle(name);
+			styles.put(name, style);
+			targets.forEach(target -> applyStyleTo(name, target));
+		} else {
+			styles.put(name, style);
+		}
 	}
 
 
@@ -34,7 +72,8 @@ public class StyleServiceImpl implements StyleService {
 
 	@Override
 	public void deregisterStyle(final String name) {
-
+		getTargets(name).forEach(target -> removeStyleFrom(name, target));
+		styles.remove(name);
 	}
 
 
@@ -42,7 +81,23 @@ public class StyleServiceImpl implements StyleService {
 
 	@Override
 	public void applyStyleTo(final String name, final Node target) {
+		Validations.INPUT.notBlank(name).exception("The name can not be null or empty.");
+		Validations.INPUT.notNull(target).exception("The target node can not be null.");
+		final Optional<Style> styleOptional = findStyle(name);
+		styleOptional.ifPresentOrElse(style -> {
+			style.applyTo(target);
+			targets.computeIfAbsent(target, k -> new ArrayList<>()).add(style);
+		}, () -> log.warn("The style '{}' does not exist and will not be applied to the given target.", name));
+	}
 
+
+
+
+	@Override
+	public void applyStylesTo(final List<String> names, final Node target) {
+		Validations.INPUT.notNull(names).exception("The names can not be null.");
+		Validations.INPUT.notNull(target).exception("The target node can not be null.");
+		names.forEach(name -> applyStyleTo(name, target));
 	}
 
 
@@ -50,7 +105,13 @@ public class StyleServiceImpl implements StyleService {
 
 	@Override
 	public void applyStyleToExclusive(final String name, final Node target) {
-
+		Validations.INPUT.notBlank(name).exception("The name can not be null or empty.");
+		Validations.INPUT.notNull(target).exception("The target node can not be null.");
+		final Optional<Style> styleOptional = findStyle(name);
+		styleOptional.ifPresentOrElse(style -> {
+			style.applyExclusive(target);
+			targets.put(target, new ArrayList<>(List.of(style)));
+		}, () -> log.warn("The style '{}' does not exist and will not be applied to the given target.", name));
 	}
 
 
@@ -58,7 +119,13 @@ public class StyleServiceImpl implements StyleService {
 
 	@Override
 	public void removeStyleFrom(final String name, final Node target) {
-
+		Validations.INPUT.notBlank(name).exception("The name can not be null or empty.");
+		Validations.INPUT.notNull(target).exception("The target node can not be null.");
+		final Optional<Style> styleOptional = findStyle(name);
+		styleOptional.ifPresentOrElse(style -> {
+			style.removeFrom(target);
+			targets.computeIfAbsent(target, k -> new ArrayList<>()).remove(style);
+		}, () -> log.warn("The style '{}' does not exist and will not be removed from the given target.", name));
 	}
 
 
@@ -66,7 +133,8 @@ public class StyleServiceImpl implements StyleService {
 
 	@Override
 	public void disconnectNode(final Node node) {
-
+		getAppliedStyles(node).forEach(style -> style.removeFrom(node));
+		targets.remove(node);
 	}
 
 
@@ -74,7 +142,14 @@ public class StyleServiceImpl implements StyleService {
 
 	@Override
 	public void reloadStyle(final String name) {
-
+		final Optional<Style> styleOptional = findStyle(name);
+		styleOptional.ifPresentOrElse(style -> {
+			final List<Node> targets = getTargets(name);
+			targets.forEach(target -> {
+				style.removeFrom(target);
+				style.applyTo(target);
+			});
+		}, () -> log.warn("The style '{}' does not exist and can not be reloaded.", name));
 	}
 
 
@@ -82,7 +157,10 @@ public class StyleServiceImpl implements StyleService {
 
 	@Override
 	public void reloadAll() {
-
+		targets.forEach((target, styles) -> {
+			styles.forEach(style -> style.removeFrom(target));
+			styles.forEach(style -> style.applyTo(target));
+		});
 	}
 
 
@@ -90,7 +168,53 @@ public class StyleServiceImpl implements StyleService {
 
 	@Override
 	public Optional<Style> findStyle(final String name) {
-		return Optional.empty();
+		return Optional.ofNullable(styles.get(name));
 	}
+
+
+
+
+	@Override
+	public List<Style> getAppliedStyles(final Node target) {
+		return Optional.ofNullable(targets.get(target)).orElse(List.of());
+	}
+
+
+
+
+	@Override
+	public List<String> getAppliedStyleNames(final Node target) {
+		return getAppliedStyles(target).stream().map(this::getNameOfStyle).collect(Collectors.toList());
+	}
+
+
+
+
+	/**
+	 * @param style the {@link Style}
+	 * @return the name of the registered style.
+	 */
+	private String getNameOfStyle(final Style style) {
+		return styles.entrySet().stream()
+				.filter(entry -> entry.getValue().equals(style))
+				.map(Map.Entry::getKey)
+				.findFirst().orElse(null);
+	}
+
+
+
+
+	@Override
+	public List<Node> getTargets(final String styleName) {
+		final List<Node> nodes = new ArrayList<>();
+		final Optional<Style> styleOptional = findStyle(styleName);
+		styleOptional.ifPresent(style -> targets.forEach((node, styles) -> {
+			if (styles.contains(style)) {
+				nodes.add(node);
+			}
+		}));
+		return nodes;
+	}
+
 
 }
